7b2692b79bfcf2fae2320eb79ab57e3e581e351e
Dino Gržinić <grzinicdino@gmail.com>
2024-09-13 00:29:39 +0200

    Increased dataset size and modified yolov5 parameters

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # SpriteNet
 
-A machine learning model for object detection and localization of sprites using the YOLOv5 model. The model is trained on
-images from the Global Meteor Network's camera and the dataset was annotated by hand.
+<a href="https://universe.roboflow.com/gmnsprites/spritenet">
+    <img src="https://app.roboflow.com/images/download-dataset-badge.svg"></img>
+</a>
+
+A machine learning model for object detection of sprites using the YOLOv5 model. The model is trained on images from the [Global Meteor Network](https://globalmeteornetwork.org/)'s camera and the dataset was annotated by hand.
+
+![Sample Image](FF_HR0002_20200920_190200_303_0130816_DETECT.png)
--- scripts/fits_converter.py
+++ scripts/fits_converter.py
@@ -1,14 +1,13 @@
 from astropy.io import fits
 import os
 from PIL import Image
-import numpy as np
 
 """
 Python script for converting FITS files into PNGs by extracting the detection only image by subtracting maxpixel and average pixel images.
 """
 
 while True:
-    converted=0
+    converted = 0
     folder = input("Enter folder: ")
     if not os.path.exists(folder):
         print("Folder does not exist!")
@@ -27,24 +26,20 @@ while True:
             print("Already exists.")
             continue
         try:
-            hdul = fits.open(os.path.join(folder, filename))
+            with fits.open(os.path.join(folder, filename)) as hdul:
+                try:
+                    maxpix = hdul[1].data
+                    avgpix = hdul[3].data
+                except TypeError:
+                    print("Possibly corrupted FITS file.")
+                    continue
+
+                detection = maxpix - avgpix
+                im = Image.fromarray(detection)
+                im.save(os.path.join(new_folder, filename + ".png"))
+                converted += 1
+                print("Success!")
         except OSError:
             print("Not a FITS file.")
-            continue
-
-        try:
-            maxpix = hdul[1].data
-            avgpix = hdul[3].data
-        except TypeError:
-            print("Possibly corrupted FITS file.")
-            continue
-
-        detection = maxpix - avgpix
-
-        im = Image.fromarray(detection)
-        im.save(os.path.join(new_folder, filename + ".png"))
-        hdul.close()
-        converted+=1
-        print("Success!")
 
     print(f"\nDone! Converted {converted} FITS files.\n")
