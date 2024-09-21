1e299a4510ba17580e20a0fbd6daa41f29193969
Dino Gržinić <grzinicdino@gmail.com>
2024-09-21 23:51:11 +0200

    added modularity to fits converter

--- scripts/fits_converter.py
+++ scripts/fits_converter.py
@@ -6,40 +6,54 @@ from PIL import Image
 Python script for converting FITS files into PNGs by extracting the detection only image by subtracting maxpixel and average pixel images.
 """
 
-while True:
+
+def fits_to_png(filename, new_folder, detection_only):
+    if ".bin" in filename:
+        with open(os.path.join(folder, filename), "rb") as fid:
+            # TODO
+            return
+
+    print("Converting:", filename, end=" -> ")
+    if os.path.isfile(os.path.join(new_folder, filename + ".png")):
+        print("Already exists.")
+        return
+    try:
+        with fits.open(os.path.join(folder, filename)) as hdul:
+            try:
+                maxpix = hdul[1].data
+                avgpix = hdul[3].data
+            except TypeError:
+                print("Possibly corrupted FITS file.")
+                return
+            if detection_only:
+                detection = maxpix - avgpix
+            else:
+                detection = maxpix
+            im = Image.fromarray(detection)
+            im.save(os.path.join(new_folder, filename + ".png"))
+            converted += 1
+            print("Success!")
+    except OSError:
+        print("Not a FITS file.")
+
+
+def folder_walker(folder, detection_only=True):
     converted = 0
-    folder = input("Enter folder: ")
     if not os.path.exists(folder):
-        print("Folder does not exist!")
-        exit()
+        return None
     new_folder = os.path.join(folder, "converted")
     if not os.path.exists(new_folder):
         os.mkdir(new_folder)
     for filename in os.listdir(folder):
-        if ".bin" in filename:
-            with open(os.path.join(folder, filename), "rb") as fid:
-                # TODO
-                continue
+        fits_to_png(filename, new_folder, detection_only)
+    return converted
 
-        print("Converting:", filename, end=" -> ")
-        if os.path.isfile(os.path.join(new_folder, filename + ".png")):
-            print("Already exists.")
+
+if __name__ == "__main__":
+    while True:
+        folder = input("Enter folder: ")
+        converted = folder_walker(folder)
+        if converted is None:
+            print("Folder does not exist.\n")
             continue
-        try:
-            with fits.open(os.path.join(folder, filename)) as hdul:
-                try:
-                    maxpix = hdul[1].data
-                    avgpix = hdul[3].data
-                except TypeError:
-                    print("Possibly corrupted FITS file.")
-                    continue
-
-                detection = maxpix - avgpix
-                im = Image.fromarray(detection)
-                im.save(os.path.join(new_folder, filename + ".png"))
-                converted += 1
-                print("Success!")
-        except OSError:
-            print("Not a FITS file.")
-
-    print(f"\nDone! Converted {converted} FITS files.\n")
+        print(f"\nDone! Converted {converted} FITS files.\n")
